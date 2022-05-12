041d73375fffef82d9892854862f1b219b69fd2a
SusanMageto <susan.mageto@student.moringaschool.com>
2022-05-12 12:39:20 +0300

    adds the email file

---
+++ app/email.py
@@ -0,0 +1,10 @@
+from flask import Message
+from flask import render_template
+from . import mail
+
+def mail_message(subject,template,to,**kwargs):
+  sender_mail = 'susanmageto.mageto@gmail.com'
+  email = Message(subject, sender = sender_mail, recipient = [to])
+  email.body = render_template(template + ".text",**kwargs)
+  email.html = render_template(template + ".html",**kwargs)
+  mail.send(email) 
